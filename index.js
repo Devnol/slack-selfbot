b11107c6bbf81e4a1e16ff0d8778c4b2ce787f53
Devnol <pgkoutsoumanis@gmail.com>
2021-02-01 16:05:59 +0200

    Add code to win counttoamillion :)
    Signed-off-by: Devnol <pgkoutsoumanis@gmail.com>

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ const app = new App({
     token: config.slack_token,
     appToken: config.app_token,
     socketMode: true,
+    ignoreSelf: false,
 });
 
 (async () => {
     await app.start()
     console.log('⚡️ Server ready!')
-    
+    console.log(Date.now()) 
 })()
 
-app.message(async ({ message, say }) => {
+app.event('message', async ({ message, say }) => {
 
-    //await say(`Hello, <@${message.user}>! I am Undefined, Devnol's personal selfbot, Nice to meet you! (j/k I'm a load of specifically polarised capacitors. Neither do I know nor do I care who you are :) )`);
+    //await console.log(`message caught: ${message.text}`);
+    if (message.channel === config.send_channel && 
+        parseInt(message.text) && 
+        message.ts < (Date.now() / 1000 - 1) && 
+        message.user != config.ignore_uid) 
 
-    await console.log(`message caught: ${message.text}`);
+    {
+        console.log("AYY NUMBER CAUGHT")
+        await say(`${parseInt(message.text) + 1}`);
+    } /*else console.log(`Message caught: ${message.text} in: ${message.channel}\n  from: ${message.user} new: ${message.ts >= (Date.now() / 1000 - 1)} \n time: ${Date.now() / 1000} msgts: ${message.ts + 1}`)*/
 
 });
